03f1bd01f235c4008114f18ffd2b0850f902c651
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-06-28 14:02:29 -0400

    fixing spacing

--- db/migrate/20230626173058_create_simulations.rb
+++ db/migrate/20230626173058_create_simulations.rb
@@ -1,14 +1,14 @@
 class CreateSimulations < ActiveRecord::Migration[7.0]
   def change
     create_table :simulations do |t|
-    	t.sting :project
-    	t.strin :pvsyst_version
-    	t.string :geographical_site
-    	t.string :meteo_data
-    	t.string :satelite_data
-    	t.string :simulation_variant
-    	t.datetime :simulation_date
-    	t.string :simulation_hourly_values
+      t.sting :project
+      t.strin :pvsyst_version
+      t.string :geographical_site
+      t.string :meteo_data
+      t.string :satelite_data
+      t.string :simulation_variant
+      t.datetime :simulation_date
+      t.string :simulation_hourly_values
       t.timestamps
     end
   end
