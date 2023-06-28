e14f8c7a842c93e834e97cc74f18d3d276c059b8
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-06-28 17:47:30 -0400

    fixing simulation. add model.

--- db/migrate/20230626173058_create_simulations.rb
+++ db/migrate/20230626173058_create_simulations.rb
@@ -1,8 +1,8 @@
 class CreateSimulations < ActiveRecord::Migration[7.0]
   def change
     create_table :simulations do |t|
-      t.sting :project
-      t.strin :pvsyst_version
+      t.string :project
+      t.string :pvsyst_version
       t.string :geographical_site
       t.string :meteo_data
       t.string :satelite_data
