317fbb4ca3ae9d36c005fadb084f0683d9a659f2
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-07-01 12:13:35 -0400

    adding rspec

--- db/migrate/20230626173058_create_projects.rb
+++ db/migrate/20230626173058_create_projects.rb
@@ -1,13 +1,13 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.string :project
+      t.string :project, null: false
       t.string :pvsyst_version
       t.string :geographical_site
       t.string :meteo_data
       t.string :satelite_data
       t.string :simulation_variant
-      t.datetime :simulation_date
+      t.datetime :simulation_date, null: false
       t.string :simulation_hourly_values
       t.timestamps
     end
