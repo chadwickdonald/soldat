2800ae3cd01dea29773e6f99354888a49573f3b8
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-07-10 23:34:24 -0400

    spec fix

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,8 +3,20 @@
 require 'spec_helper'
 
 describe Project do
+	fixtures :projects
+
+	before(:context) do
+		project = projects(:default)
+	end
+
   it "is valid with valid attributes"
-  it "is not valid without a project"
+  it "is not valid without a project" do
+  	# project = projects(:default)
+  	puts "---project: #{project.inspect}"
+  	# expect(project.project).to be_valid
+  	# it { is_expected.to validate_presence_of :project }
+  end
+
   it "is not valid without a pvsyst_version"
   it "is not valid without a geographical_site"
   it "is not valid without a meteo_data"
@@ -13,15 +25,3 @@ describe Project do
   it "is not valid without a simulation_variant"
   it "is not valid without a simulation_hourly_values"
 end
-
-
-# RSpec.describe Project, type: :model do
-# 	subject { Project.new(project: 'Test_IEA_GA_Perry.PRJ', 
-# 												pvsyst_version: 'v6.86',
-# 												geographical_site: '32_36  -83_77_SA_Perry_GA_TMY.SIT',
-# 												meteo_data: '32_36  -83_77_SA_Perry_GA_TMY.MET',
-# 												satelite_data: 'SUNY model;TMY',
-# 												simulation_variant: 'IEA_GA_Perry.VC9',
-# 												simulation_date: '04/06/20 16h21',
-# 												simulation_hourly_values:'from 01/01/90 to 31/12/90' ) }
-# end
