0e885c8dc538fab3983a785163c56e58ec964143
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-07-13 16:31:09 -0700

    adding relationships

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+	has_many :pvsyst_simulations, dependent: :destroy
 	validates :project, presence: true
 	validates :pvsyst_version, presence: true
 	validates :geographical_site, presence: true
--- app/models/pvsyst_simulation.rb
+++ app/models/pvsyst_simulation.rb
@@ -1,2 +1,3 @@
 class PvsystSimulation < ApplicationRecord
+	belongs_to :project
 end
