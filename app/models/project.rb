a260c488c51752069df3cd1271e6aecbf55e9286
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-07-14 13:07:31 -0700

    removing file

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
 	has_many :pvsyst_simulations, dependent: :destroy
+
 	validates :project, presence: true
 	validates :pvsyst_version, presence: true
 	validates :geographical_site, presence: true
