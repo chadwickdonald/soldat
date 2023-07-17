15008e4b9811f1fc4e55307f63011f0f534b177a
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-07-16 20:07:24 -0700

    adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'home/index'
+  root "home#index"
   resources :projects, only: [:index]
   resources :pvsysts, only: [:index]
 end
