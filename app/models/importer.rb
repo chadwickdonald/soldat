26b0a5575715b537b9af96c64c1d494caa258197
Chadwick Bidwell <chadwickbidwell@gmail.com>
2023-08-13 14:16:05 -0700

    fixing importer

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -10,7 +10,6 @@ class Importer
     puts "---file: #{file.inspect}"
     project = Project.new
     File.foreach(file.path).with_index do |line, index|
-      puts "---index: #{index}, line: #{line}"
       begin
         file_line = line.split(',')
       rescue => exception
@@ -48,38 +47,43 @@ class Importer
           end
         end
       end
-      # if index > 13
-      if [21, 22, 23].include?(index)
-      # if index == 21
-        pvsyst = PvsystSimulation.new
-        pvsyst.project_id = project.id
-        pvsyst.simulation_time = fix_date(file_line[0])
-        pvsyst.glob_hor = file_line[1].to_f
-        pvsyst.diff_hor = file_line[2].to_f
-        pvsyst.beam_hor = file_line[3].to_f
-        pvsyst.t_amb = file_line[4].to_f
-        pvsyst.wind_vel = file_line[5].to_f
-        pvsyst.glob_inc = file_line[6].to_f
-        pvsyst.shd_loss = file_line[7].to_f
-        pvsyst.iam_loss = file_line[8].to_f
-        pvsyst.slg_loss = file_line[9].to_f
-        pvsyst.glob_eff = file_line[10].to_f
-        pvsyst.e_arr_nom = file_line[11].to_f
-        pvsyst.g_inc_lss = file_line[12].to_f
-        pvsyst.temp_lss = file_line[13].to_f
-        pvsyst.mod_qual = file_line[14].to_f
-        pvsyst.mis_loss = file_line[15].to_f
-        pvsyst.ohm_loss = file_line[16].to_f
-        pvsyst.e_arr_mpp = file_line[17].to_f
-        pvsyst.e_array = file_line[18].to_f
-        pvsyst.t_array = file_line[19].to_f
-        pvsyst.il_oper = file_line[20].to_f
-        pvsyst.il_pmin = file_line[21].to_f
-        pvsyst.il_pmax = file_line[22].to_f
-        pvsyst.il_vmin = file_line[23].to_f
-        pvsyst.e_out_inv =file_line[24].to_f
-        pvsyst.e_grid = file_line[25].to_f
-        pvsyst.u_array = file_line[26].to_f
+      if index >= 14 && index <= 8773
+        begin
+          pvsyst = PvsystSimulation.new
+          pvsyst.project_id = project.id
+          pvsyst.simulation_time = fix_date(file_line[0])
+          pvsyst.glob_hor = file_line[1].to_f
+          pvsyst.diff_hor = file_line[2].to_f
+          pvsyst.beam_hor = file_line[3].to_f
+          pvsyst.t_amb = file_line[4].to_f
+          pvsyst.wind_vel = file_line[5].to_f
+          pvsyst.glob_inc = file_line[6].to_f
+          pvsyst.shd_loss = file_line[7].to_f
+          pvsyst.iam_loss = file_line[8].to_f
+          pvsyst.slg_loss = file_line[9].to_f
+          pvsyst.glob_eff = file_line[10].to_f
+          pvsyst.e_arr_nom = file_line[11].to_f
+          pvsyst.g_inc_lss = file_line[12].to_f
+          pvsyst.temp_lss = file_line[13].to_f
+          pvsyst.mod_qual = file_line[14].to_f
+          pvsyst.mis_loss = file_line[15].to_f
+          pvsyst.ohm_loss = file_line[16].to_f
+          pvsyst.e_arr_mpp = file_line[17].to_f
+          pvsyst.e_array = file_line[18].to_f
+          pvsyst.t_array = file_line[19].to_f
+          pvsyst.il_oper = file_line[20].to_f
+          pvsyst.il_pmin = file_line[21].to_f
+          pvsyst.il_pmax = file_line[22].to_f
+          pvsyst.il_vmin = file_line[23].to_f
+          pvsyst.e_out_inv =file_line[24].to_f
+          pvsyst.e_grid = file_line[25].to_f
+          pvsyst.u_array = file_line[26].to_f
+        rescue => exception
+          puts "---hourly record exception: #{exception.inspect}"
+          puts exception.backtrace
+          break
+        end
+
         begin
           pvsyst.save!
         rescue => exception
@@ -352,6 +356,7 @@ class Importer
     date_2 = date_2.join('/')
     date_1[0] = date_2
     date_1 = date_1.join(' ')
-    date_1.to_datetime
+    DateTime.strptime(date_1, "%m/%d/%Y")
   end
+
 end
