efc9f1b0f87acdcf2cc0ddbf622f780962aa8eba
Chadwick Bidwell <chadwickbidwell@gmail.com>
2024-08-19 11:03:50 -0700

    removing comment

--- app/models/scada_organization.rb
+++ app/models/scada_organization.rb
@@ -1,4 +1,3 @@
-# app/models/scada_organization.rb
 class ScadaOrganization < ApplicationRecord
   has_many :scada_sites, primary_key: 'id', foreign_key: 'organization_id'
 end
--- app/models/scada_site.rb
+++ app/models/scada_site.rb
@@ -1,4 +1,3 @@
-# app/models/scada_site.rb
 class ScadaSite < ApplicationRecord
   has_many :scada_segments, primary_key: 'uuid', foreign_key: 'site_id'
   belongs_to :scada_organization, primary_key: 'id', foreign_key: 'organization_id'
