b021f7d2f45837903cab5b9ba6c453663ff92daf
Chadwick Bidwell <chadwickbidwell@gmail.com>
2024-08-18 17:44:59 -0700

    spacing

--- app/models/scada_organization.rb
+++ app/models/scada_organization.rb
@@ -1,4 +1,4 @@
 # app/models/scada_organization.rb
 class ScadaOrganization < ApplicationRecord
-	has_many :scada_sites, primary_key: 'id', foreign_key: 'organization_id'
+  has_many :scada_sites, primary_key: 'id', foreign_key: 'organization_id'
 end
