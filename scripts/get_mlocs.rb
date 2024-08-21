f71c5ba2e31b831ce5300804f81c3e8a3a50a29e
Chadwick Bidwell <chadwickbidwell@gmail.com>
2024-08-20 18:55:16 -0700

    updating to check if mloc exists

--- scripts/get_mlocs.rb
+++ scripts/get_mlocs.rb
@@ -4,8 +4,7 @@ require 'json'
 require_relative '../config/environment'
 
 segment_uuids = ScadaSegment.pluck(:uuid)
-
-api_key = '4babde93-07c2-428c-9bd4-6f04b038afe1.01'
+api_key = SCADA_API_KEY
 
 segment_uuids.each do |segment_id|
   uri = URI("https://portal.solarpark-online.com/ifms/segments/#{segment_id}/mlocs")
@@ -19,15 +18,19 @@ segment_uuids.each do |segment_id|
   if response.is_a?(Net::HTTPSuccess)
     mlocs = JSON.parse(response.body)
     mlocs.each do |mloc|
-      ScadaMloc.create!(
-        segment_id: segment_id,
-        apcode: mloc['apcode'],
-        uuid: mloc['id'],
-        name: mloc['name'],
-        sscode: mloc['sscode'],
-        uri: mloc['uri'],
-        measurementTypeId: mloc['measurementTypeId']
-      )
+      unless ScadaMloc.exists?(uuid: mloc['id'])
+        ScadaMloc.create!(
+          segment_id: segment_id,
+          apcode: mloc['apcode'],
+          uuid: mloc['id'],
+          name: mloc['name'],
+          sscode: mloc['sscode'],
+          uri: mloc['uri'],
+          measurementTypeId: mloc['measurementTypeId']
+        )
+      else
+        puts "Mloc with UUID #{mloc['id']} already exists. Skipping..."
+      end
     end
   else
     puts "Error: #{response.message} for segment_id: #{segment_id}"
