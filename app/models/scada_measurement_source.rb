cc3e52abd3dff8b1f23de7d08f5886d201dcf2fb
Chadwick Bidwell <chadwickbidwell@gmail.com>
2024-08-30 23:10:59 -0700

    changing relationship to has_many

--- app/models/scada_measurement_source.rb
+++ app/models/scada_measurement_source.rb
@@ -1,4 +1,4 @@
 class ScadaMeasurementSource < ApplicationRecord
   belongs_to :scada_measurement, primary_key: 'id', foreign_key: 'scada_measurement_id'
-  has_one :scada_event, primary_key: 'uuid', foreign_key: 'measurement_source_id'
+  has_many :scada_events, primary_key: 'uuid', foreign_key: 'measurement_source_id'
 end
