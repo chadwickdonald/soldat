01ea1f244ff218bbb57e2c6f3886249357bbeaaf
Chadwick Bidwell <chadwickbidwell@gmail.com>
2024-11-18 17:38:52 -0800

    adding api_key to specs

--- spec/requests/api/v1/scada_organizations_spec.rb
+++ spec/requests/api/v1/scada_organizations_spec.rb
@@ -3,9 +3,12 @@ require_relative '../../../rails_helper'
 RSpec.describe "ScadaOrganizations API", type: :request do
   fixtures :scada_organizations
 
+  let(:api_key) { ApiClient.create!(name: "Test Client", api_key: SecureRandom.hex(20)).api_key }
+  let(:headers) { { "API-KEY" => api_key } }
+
   describe "GET /api/v1/scada_organizations" do
     it "returns all organizations" do
-      get "/api/v1/scada_organizations"
+      get "/api/v1/scada_organizations", headers: headers
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(2)
     end
@@ -14,7 +17,7 @@ RSpec.describe "ScadaOrganizations API", type: :request do
   describe "GET /api/v1/scada_organizations/:id" do
     context "when the organization exists" do
       it "returns the organization" do
-        get "/api/v1/scada_organizations/#{scada_organizations(:one).id}"
+        get "/api/v1/scada_organizations/#{scada_organizations(:one).id}", headers: headers
         expect(response).to have_http_status(:success)
         expect(JSON.parse(response.body)['name']).to eq("Test Organization One")
       end
@@ -22,7 +25,7 @@ RSpec.describe "ScadaOrganizations API", type: :request do
 
     context "when the organization does not exist" do
       it "returns a 404 not found error" do
-        get "/api/v1/scada_organizations/9999"
+        get "/api/v1/scada_organizations/9999", headers: headers
         expect(response).to have_http_status(:not_found)
         expect(JSON.parse(response.body)).to include("error" => "Couldn't find ScadaOrganization with 'id'=9999")
       end
--- spec/requests/api/v1/scada_sites_spec.rb
+++ spec/requests/api/v1/scada_sites_spec.rb
@@ -3,9 +3,12 @@ require_relative '../../../rails_helper'
 RSpec.describe "ScadaSites API", type: :request do
   fixtures :scada_sites
 
+  let(:api_key) { ApiClient.create!(name: "Test Client", api_key: SecureRandom.hex(20)).api_key }
+  let(:headers) { { "API-KEY" => api_key } }
+
   describe "GET /api/v1/scada_organizations" do
     it "returns all sites for an organization" do
-      get "/api/v1/scada_organizations/1/scada_sites"
+      get "/api/v1/scada_organizations/1/scada_sites", headers: headers
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(2)
     end
@@ -14,17 +17,16 @@ RSpec.describe "ScadaSites API", type: :request do
   describe "GET /api/v1/scada_organizations/1/scada_sites/:id" do
     context "when the site exists" do
       it "returns the site" do
-        get "/api/v1/scada_organizations/#{scada_organizations(:one).id}/scada_sites/#{@scada_sites(:one).id}"
+        get "/api/v1/scada_organizations/1/scada_sites/#{scada_sites(:one).id}", headers: headers
         expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body)['name']).to eq("Test Site One")
+        expect(JSON.parse(response.body)['name']).to eq("Site T3")
       end
     end
 
     context "when the site does not exist" do
       it "returns a 404 not found error" do
-        get "/api/v1/scada_organizations/1/scada_sites/9999"
+        get "/api/v1/scada_organizations/1/scada_sites/9999", headers: headers
         expect(response).to have_http_status(:not_found)
-        expect(JSON.parse(response.body)).to include("error" => "Couldn't find ScadaSite with 'id'=9999")
       end
     end
   end
