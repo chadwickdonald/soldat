5c68fc0674f3bf8fda962120c5521226d6fb67e9
Chadwick Bidwell <chadwickbidwell@gmail.com>
2025-04-02 20:12:10 -0700

    removing persist method."
    exit

--- spec/services/pf/site_data_service_spec.rb
+++ spec/services/pf/site_data_service_spec.rb
@@ -23,47 +23,6 @@ RSpec.describe Pf::SiteDataService do
     end
   end
 
-  describe '#persist' do
-    let(:site_data) do
-      {
-        'id' => 'test-uuid-123',
-        'name' => 'Test Site',
-        'siteTypeApcode' => 'PVPlant',
-        'state' => 'online',
-        'enterprise' => { 'id' => 'ent-id', 'name' => 'Fake Enterprise' },
-        'properties' => {
-          'Timezone' => 'US/Central',
-          'Latitude' => '29.0',
-          'Longitude' => '-95.0',
-          'AddressStreet' => '123 Fake St.',
-          'City' => 'Houston',
-          'Country' => 'USA',
-          'Portfolio' => 'TestCo',
-          'RecordingPeriod' => '5m',
-          'NumOfTCPs' => '42'
-        }
-      }
-    end
-
-    it 'creates a new ScadaSite if it does not exist' do
-      expect {
-        service.persist(site_data)
-      }.to change(ScadaSite, :count).by(1)
-
-      scada_site = ScadaSite.last
-      expect(scada_site.uuid).to eq('test-uuid-123')
-      expect(scada_site.name).to eq('Test Site')
-      expect(scada_site.latitude).to eq('29.0')
-    end
-
-    it 'does not create a duplicate ScadaSite' do
-      ScadaSite.create!(uuid: 'test-uuid-123', name: 'Existing', organization_id: 1)
-      expect {
-        service.persist(site_data)
-      }.not_to change(ScadaSite, :count)
-    end
-  end
-
   describe '#fetch_and_persist_sites' do
     it 'fetches and persists site data', :vcr do
       expect {
