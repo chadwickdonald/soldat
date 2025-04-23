6f3b597a46a604ab8ee94d440c9f9a968363034e
Chadwick Bidwell <chadwickbidwell@gmail.com>
2025-04-22 22:38:33 -0700

    adding scada_mloc persist_from_pf and spec

--- app/models/scada_mloc.rb
+++ app/models/scada_mloc.rb
@@ -1,4 +1,21 @@
 class ScadaMloc < ApplicationRecord
   has_many :scada_measurements, primary_key: 'uuid', foreign_key: 'mloc_id'
   belongs_to :scada_segment, primary_key: 'uuid', foreign_key: 'segment_id'
+
+  def self.persist_from_pf(mloc_data, segment_id)
+
+    uuid = mloc_data['id']
+
+    return if exists?(uuid: uuid)
+
+    create!(
+      uuid: uuid,
+      segment_id: segment_id,
+      apcode: mloc_data['apcode'],
+      name: mloc_data['name'],
+      sscode: mloc_data['sscode'],
+      uri: mloc_data['uri'],
+      measurementTypeId: mloc_data['measurementTypeId']
+    )
+  end
 end
---
+++ spec/models/scada_mloc_spec.rb
@@ -0,0 +1,43 @@
+# spec/models/scada_mloc_spec.rb
+require 'rails_helper'
+
+RSpec.describe ScadaMloc, type: :model do
+  describe '.persist_from_pf' do
+    let!(:segment) { ScadaSegment.create!(uuid: "b20ab912-8854-11ee-a4ff-42010afa015a", site_id: "25658d43-0ffd-42b4-a4e4-d3b808e85087", name: "Tracker TCU-073-006") }
+
+    let(:mloc_data) do
+      {
+        'id' => "5af838ce-8855-11ee-a4ff-42010afa015a",
+        'segmentId' => segment.uuid,
+        'apcode' => 'TrackerIDNumber',
+        'name' => 'ID number',
+        'sscode' => nil,
+        'uri' => 'http://portal.example.com/mlocs/mloc-uuid-123',
+        'measurementTypeId' => '3074ed08-8264-11de-ad55-0090f586a869'
+      }
+    end
+
+    it 'creates a new ScadaMloc if it does not exist' do
+      expect {
+        ScadaMloc.persist_from_pf(mloc_data, segment.uuid)
+      }.to change(ScadaMloc, :count).by(1)
+
+      mloc = ScadaMloc.last
+      expect(mloc.uuid).to eq("5af838ce-8855-11ee-a4ff-42010afa015a")
+      expect(mloc.name).to eq('ID number')
+    end
+
+    before do
+      ScadaMloc.destroy_all
+    end
+
+    it 'does not create a duplicate ScadaMloc' do
+      ScadaMloc.create!(uuid: "5af838ce-8855-11ee-a4ff-42010afa015a", segment_id: segment.uuid, name: 'Existing MLOC')
+      
+      expect {
+        ScadaMloc.persist_from_pf(mloc_data, segment.uuid)
+
+      }.not_to change(ScadaMloc, :count)
+    end
+  end
+end
