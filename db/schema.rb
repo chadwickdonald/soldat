d2ec6f68630024336e7b526840402189f0ad6782
Chadwick Bidwell <chadwickbidwell@gmail.com>
2025-10-02 11:09:35 -0700

    forgot to add changes to schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_15_032759) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_02_041524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_15_032759) do
     t.string "station_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "relevance", default: 1, null: false
   end
 
   create_table "projects", force: :cascade do |t|
